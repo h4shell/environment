c9e46f08f41b8014c0a7e87f1187b36ae49eaad8
Lorenzo Fornara <fw1987@gmail.com>
2024-05-14 15:55:31 +0200

    update gitconfig.sh

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -4,7 +4,7 @@ echo "Enter your name:"
 read GIT_NAME
 echo "Enter your email:"
 read GIT_EMAIL
-echo "[user] name = ${GIT_NAME} email = ${GIT_EMAIL}" > .gitconfig
+echo -e "[user]\n name = ${GIT_NAME}\n email = ${GIT_EMAIL}" > .gitconfig
 docker cp .gitconfig enviroment-server:/home/user/.gitconfig
 docker exec -it enviroment-server chown user:user /home/user/.gitconfig
 rm .gitconfig
