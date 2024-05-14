0cc338ce1b6d1fa06c990e7e0b1f87d5eb8356ce
Lorenzo Fornara <fw1987@gmail.com>
2024-05-14 15:45:37 +0200

    bug fix

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -1,2 +1,10 @@
-docker exec -it enviroment-server /bin/bash -c "sudo -u user git config --global user.name '${GIT_NAME}'"
-docker exec -it enviroment-server /bin/bash -c "sudo -u user git config --global user.email ${GIT_EMAIL}"
+#!/bin/bash
+
+echo "Enter your name:"
+read GIT_NAME
+echo "Enter your email:"
+read GIT_EMAIL
+echo "[user] name = ${GIT_NAME} email = ${GIT_EMAIL}" > .gitconfig
+docker cp .gitconfig enviroment-server:/home/user/.gitconfig
+docker exec -it enviroment-server chown user:user /home/user/.gitconfig
+rm .gitconfig
