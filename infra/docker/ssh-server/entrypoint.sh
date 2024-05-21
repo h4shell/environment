2749f4bc9487893b87d0383af55013335dd8ba47
Lorenzo Fornara <fw1987@gmail.com>
2024-05-21 22:24:27 +0000

    fix dockerd

--- infra/docker/ssh-server/entrypoint.sh
+++ infra/docker/ssh-server/entrypoint.sh
@@ -5,6 +5,6 @@ echo -e "[user]\n\
         > /home/user/.gitconfig
 
 chown -R user:user /home/user/
-/usr/sbin/sshd -D -f /etc/ssh/sshd_config
 
+/usr/sbin/sshd -D -f /etc/ssh/sshd_config &&
 dockerd
