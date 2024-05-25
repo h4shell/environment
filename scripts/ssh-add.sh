2de40a7f006316a2bda27a8a92db1e67714b4107
Lorenzo Fornara <fw1987@gmail.com>
2024-05-25 10:51:05 +0000

    update ssh-add.sh

--- scripts/ssh-add.sh
+++ scripts/ssh-add.sh
@@ -6,4 +6,4 @@ echo 'Host enviroment
         HostName 127.0.0.1
         User user
         Port 2222
-        IdentityFile /home/\$USER/.ssh/id_rsa_env' >> /home/$USER/.ssh/config
+        IdentityFile /home/'"$USER"'/.ssh/id_rsa_env' >> /home/$USER/.ssh/config
