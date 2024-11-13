8c5ea67ef3dca60ad94814f0bd832133de14f36a
Lorenzo Fornara <60841743+h4shell@users.noreply.github.com>
2024-11-13 13:36:59 +0100

    Update ssh-add.sh ubuntu

--- ubuntu-environment/scripts/ssh-add.sh
+++ ubuntu-environment/scripts/ssh-add.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-docker cp environment-server:/home/user/.ssh/id_rsa_env /home/$USER/.ssh/
-touch /home/$USER/.ssh/config
-echo 'Host environment
-        HostName 127.0.0.1
-        User user
-        Port 2222
-        IdentityFile /home/'"$USER"'/.ssh/id_rsa_env' >> /home/$USER/.ssh/config
+docker cp environment-server:/home/user/.ssh/id_rsa_env /home/$USER/.ssh/id_rsa_ubuntu_env
