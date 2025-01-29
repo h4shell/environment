db3c8fd1886b668650d3e7fbca9bd11841f8e090
Lorenzo Fornara <h4shell@gmail.com>
2025-01-29 21:39:17 +0000

    fix script cloudflare.sh

--- ubuntu-environment/scripts/cloudflare.sh
+++ ubuntu-environment/scripts/cloudflare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose logs cloudflare-env | grep -o 'https://[^ ]*\.trycloudflare\.com' | head -n 1 | sed 's/$/\/wetty/'
+docker compose logs cloudflare-env | grep -o 'https://[^ ]*\.trycloudflare\.com' | head -n 1 | sed 's/$/\/wetty/'
 
 
